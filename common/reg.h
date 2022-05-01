752b4e5eea54012586e688b4387dc02b50bfdc85
nikolashn <nehn.2019@gmail.com>
2022-05-01 19:52:24 +0100

    Remove rbx from the list of scratch registers

--- common/reg.h
+++ common/reg.h
@@ -11,17 +11,16 @@ enum {
 	BA_CTRREG_RAX = 1 << 0,
 	BA_CTRREG_RCX = 1 << 1,
 	BA_CTRREG_RDX = 1 << 2,
-	BA_CTRREG_RBX = 1 << 3,
-	BA_CTRREG_RSI = 1 << 4,
-	BA_CTRREG_RDI = 1 << 5,
-	BA_CTRREG_R8  = 1 << 6,
-	BA_CTRREG_R9  = 1 << 7,
-	BA_CTRREG_R10 = 1 << 8,
-	BA_CTRREG_R11 = 1 << 9,
-	BA_CTRREG_R12 = 1 << 10,
-	BA_CTRREG_R13 = 1 << 11,
-	BA_CTRREG_R14 = 1 << 12,
-	BA_CTRREG_R15 = 1 << 13
+	BA_CTRREG_RSI = 1 << 3,
+	BA_CTRREG_RDI = 1 << 4,
+	BA_CTRREG_R8  = 1 << 5,
+	BA_CTRREG_R9  = 1 << 6,
+	BA_CTRREG_R10 = 1 << 7,
+	BA_CTRREG_R11 = 1 << 8,
+	BA_CTRREG_R12 = 1 << 9,
+	BA_CTRREG_R13 = 1 << 10,
+	BA_CTRREG_R14 = 1 << 11,
+	BA_CTRREG_R15 = 1 << 12
 };
 
 u64 ba_IMToCtrReg(u64 reg) {
@@ -29,7 +28,6 @@ u64 ba_IMToCtrReg(u64 reg) {
 		case BA_IM_RAX: return BA_CTRREG_RAX;
 		case BA_IM_RCX: return BA_CTRREG_RCX;
 		case BA_IM_RDX: return BA_CTRREG_RDX;
-		case BA_IM_RBX: return BA_CTRREG_RBX;
 		case BA_IM_RSI: return BA_CTRREG_RSI;
 		case BA_IM_RDI: return BA_CTRREG_RDI;
 		case BA_IM_R8:  return BA_CTRREG_R8;
@@ -49,7 +47,6 @@ u64 ba_CtrRegToIM(u64 reg) {
 		case BA_CTRREG_RAX: return BA_IM_RAX;
 		case BA_CTRREG_RCX: return BA_IM_RCX;
 		case BA_CTRREG_RDX: return BA_IM_RDX;
-		case BA_CTRREG_RBX: return BA_IM_RBX;
 		case BA_CTRREG_RSI: return BA_IM_RSI;
 		case BA_CTRREG_RDI: return BA_IM_RDI;
 		case BA_CTRREG_R8:  return BA_IM_R8;
