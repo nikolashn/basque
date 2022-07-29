0440e512293cbebe3bddb6e2ae2f2c0d033ec1dd
nikolashn <nehn.2019@gmail.com>
2022-07-29 21:50:36 +0100

    Add write syscall

--- bltin/bltinflags.h
+++ bltin/bltinflags.h
@@ -4,13 +4,14 @@
 #define BA__BLTINFLAGS_H
 
 enum {
-	BA_BLTIN_U64ToStr = 0,
-	BA_BLTIN_MemCopy  = 1,
+	BA_BLTIN_U64ToStr   = 0,
+	BA_BLTIN_MemCopy    = 1,
 	
-	BA_BLTIN_Sys      = 2,
-	BA_BLTIN_SysRead  = 3,
+	BA_BLTIN_Sys        = 2,
+	BA_BLTIN_SysRead    = 3,
+	BA_BLTIN_SysWrite   = 4,
 	
-	BA_BLTIN__COUNT   = 4
+	BA_BLTIN__COUNT     = 5
 };
 
 u64 ba_BltinFlags[1] = {0};
--- bltin/sys.h
+++ bltin/sys.h
@@ -7,7 +7,8 @@
 
 /* Wrappers for syscalls */
 
-/* Params: buf (0x8), count (0x8), fd (0x8)
+/* syscall read
+ * Params: buf (0x8), count (0x8), fd (0x8)
  * Returns: (rax) no. of bytes read or -1 if err */
 void ba_BltinSysRead(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysRead);
@@ -35,6 +36,38 @@ void ba_BltinSysRead(struct ba_Ctr* ctr) {
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBX); // Push return location
 	ba_AddIM(ctr, 1, BA_IM_RET);
 }
+
+/* syscall write
+ * Params: buf (0x8), count (0x8), fd (0x8)
+ * Returns: (rax) no. of bytes written or -1 if err */
+void ba_BltinSysWrite(struct ba_Ctr* ctr) {
+	ba_BltinFlagsSet(BA_BLTIN_SysWrite);
+	ba_BltinLabels[BA_BLTIN_SysWrite] = ctr->labelCnt;
+	++ctr->labelCnt;
+	
+	// --- sys.Write ---
+	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
+	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
+	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
+	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RDI);
+	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RSI);
+	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RDX);
+	ba_AddIM(ctr, 3, BA_IM_MOV, BA_IM_RBP, BA_IM_RSP);
+	ba_AddIM(ctr, 4, BA_IM_MOV, BA_IM_RAX, BA_IM_IMM, 1);
+	// RDI=fd, RSI=buf, RDX=count
+	ba_AddIM(ctr, 5, BA_IM_MOV, BA_IM_RSI, BA_IM_ADRADD, BA_IM_RBP, 0x30);
+	ba_AddIM(ctr, 5, BA_IM_MOV, BA_IM_RDX, BA_IM_ADRADD, BA_IM_RBP, 0x28);
+	ba_AddIM(ctr, 5, BA_IM_MOV, BA_IM_RDI, BA_IM_ADRADD, BA_IM_RBP, 0x20);
+	ba_AddIM(ctr, 1, BA_IM_SYSCALL);
+	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RDX);
+	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RSI);
+	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RDI);
+	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBP); // Restore rbp
+	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBX); // Push return location
+	ba_AddIM(ctr, 1, BA_IM_RET);
+}
+
+/* Including all the syscalls */
 
 struct ba_Func* ba_IncludeSysAddFunc(struct ba_Ctr* ctr, u64 line, u64 col, 
 	char* funcName) 
@@ -105,6 +138,46 @@ void ba_IncludeSys(struct ba_Ctr* ctr, u64 line, u64 col) {
 		params[2]->type = (struct ba_Type){ BA_TYPE_I64, 0 };
 		params[2]->hasDefaultVal = 1;
 		params[2]->defaultVal = 0; // sys.FD_STDIN
+		params[1]->next = params[2];
+	}
+	if (!ba_BltinFlagsTest(BA_BLTIN_SysWrite)) {
+		struct ba_Func* func = ba_IncludeSysAddFunc(ctr, line, col, "Write");
+		struct ba_IM* oldIM = ctr->im;
+		ctr->im = func->imBegin;
+		ba_BltinSysWrite(ctr);
+		func->imEnd = ctr->im;
+		ctr->im = oldIM;
+
+		func->retType = (struct ba_Type){ BA_TYPE_I64, 0 };
+		func->lblStart = ba_BltinLabels[BA_BLTIN_SysWrite];
+		func->isCalled = 0;
+		func->doesReturn = 1;
+		func->paramCnt = 3;
+		func->paramStackSize = 0x18;
+
+		struct ba_FuncParam* params[3];
+
+		// buf (RSI)
+		params[0] = ba_NewFuncParam();
+		{
+			struct ba_Type* fundType = malloc(sizeof(*fundType));
+			fundType->type = BA_TYPE_VOID;
+			params[0]->type = (struct ba_Type){ BA_TYPE_PTR, fundType };
+		}
+		params[0]->hasDefaultVal = 0;
+		func->firstParam = params[0];
+
+		// count (RDX)
+		params[1] = ba_NewFuncParam();
+		params[1]->type = (struct ba_Type){ BA_TYPE_U64, 0 };
+		params[1]->hasDefaultVal = 0;
+		params[0]->next = params[1];
+
+		// fd (RDI)
+		params[2] = ba_NewFuncParam();
+		params[2]->type = (struct ba_Type){ BA_TYPE_I64, 0 };
+		params[2]->hasDefaultVal = 1;
+		params[2]->defaultVal = (void*)1; // sys.FD_STDOUT
 		params[1]->next = params[2];
 	}
 }
