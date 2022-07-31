9c6716f48761ef2acc369736ca49d1e88ccc0625
nikolashn <nehn.2019@gmail.com>
2022-07-31 17:02:52 +0100

    Add mprotect syscall

--- bltin/sys.c
+++ bltin/sys.c
@@ -12,7 +12,6 @@ void ba_BltinSysRead(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysRead);
 	ba_BltinLblSet(BA_BLTIN_SysRead, ctr->labelCnt);
 	++ctr->labelCnt;
-	
 	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
 	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
@@ -34,7 +33,6 @@ void ba_BltinSysWrite(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysWrite);
 	ba_BltinLblSet(BA_BLTIN_SysWrite, ctr->labelCnt);
 	++ctr->labelCnt;
-	
 	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
 	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
@@ -56,7 +54,6 @@ void ba_BltinSysOpen(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysOpen);
 	ba_BltinLblSet(BA_BLTIN_SysOpen, ctr->labelCnt);
 	++ctr->labelCnt;
-	
 	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
 	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
@@ -78,7 +75,6 @@ void ba_BltinSysClose(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysClose);
 	ba_BltinLblSet(BA_BLTIN_SysClose, ctr->labelCnt);
 	++ctr->labelCnt;
-	
 	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
 	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
@@ -100,7 +96,6 @@ void ba_BltinSysLSeek(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysLSeek);
 	ba_BltinLblSet(BA_BLTIN_SysLSeek, ctr->labelCnt);
 	++ctr->labelCnt;
-	
 	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
 	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
@@ -122,7 +117,6 @@ void ba_BltinSysMMap(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysMMap);
 	ba_BltinLblSet(BA_BLTIN_SysMMap, ctr->labelCnt);
 	++ctr->labelCnt;
-	
 	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
 	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
@@ -146,6 +140,27 @@ void ba_BltinSysMMap(struct ba_Ctr* ctr) {
 	ba_AddIM(ctr, 1, BA_IM_RET);
 }
 
+/* syscall mprotect
+ * Params: addr (0x8), size (0x8), prot (0x8)
+ * Returns: (rax) offset location */
+void ba_BltinSysMProtect(struct ba_Ctr* ctr) {
+	ba_BltinFlagsSet(BA_BLTIN_SysMProtect);
+	ba_BltinLblSet(BA_BLTIN_SysMProtect, ctr->labelCnt);
+	++ctr->labelCnt;
+	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
+	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
+	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
+	ba_AddIM(ctr, 3, BA_IM_MOV, BA_IM_RBP, BA_IM_RSP);
+	ba_AddIM(ctr, 4, BA_IM_MOV, BA_IM_RAX, BA_IM_IMM, 10);
+	ba_AddIM(ctr, 5, BA_IM_MOV, BA_IM_RDI, BA_IM_ADRADD, BA_IM_RBP, 0x18);
+	ba_AddIM(ctr, 5, BA_IM_MOV, BA_IM_RSI, BA_IM_ADRADD, BA_IM_RBP, 0x10);
+	ba_AddIM(ctr, 5, BA_IM_MOV, BA_IM_RDX, BA_IM_ADRADD, BA_IM_RBP, 0x08);
+	ba_AddIM(ctr, 1, BA_IM_SYSCALL);
+	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBP); // Restore rbp
+	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBX); // Push return location
+	ba_AddIM(ctr, 1, BA_IM_RET);
+}
+
 /* syscall munmap
  * Params: start (0x8), size (0x8)
  * Returns: (rax) 0 on success, -1 on failure */
@@ -153,7 +168,6 @@ void ba_BltinSysMUnmap(struct ba_Ctr* ctr) {
 	ba_BltinFlagsSet(BA_BLTIN_SysMUnmap);
 	ba_BltinLblSet(BA_BLTIN_SysMUnmap, ctr->labelCnt);
 	++ctr->labelCnt;
-	
 	ba_AddIM(ctr, 2, BA_IM_LABEL, ctr->labelCnt-1);
 	ba_AddIM(ctr, 2, BA_IM_POP, BA_IM_RBX); // Store return location in rbx
 	ba_AddIM(ctr, 2, BA_IM_PUSH, BA_IM_RBP);
@@ -407,6 +421,45 @@ void ba_IncludeSys(struct ba_Ctr* ctr, u64 line, u64 col) {
 		params[5]->hasDefaultVal = 1;
 		params[5]->defaultVal = 0;
 		params[4]->next = params[5];
+	}
+	if (!ba_BltinFlagsTest(BA_BLTIN_SysMProtect)) {
+		struct ba_Func* func = ba_IncludeAddFunc(ctr, line, col, "MProtect");
+		struct ba_IM* oldIM = ctr->im;
+		ctr->im = func->imBegin;
+		ba_BltinSysMProtect(ctr);
+		func->imEnd = ctr->im;
+		ctr->im = oldIM;
+
+		func->retType = (struct ba_Type){ BA_TYPE_I64, 0 };
+		func->lblStart = ba_BltinLblGet(BA_BLTIN_SysMProtect);
+		func->isCalled = 0;
+		func->doesReturn = 1;
+		func->paramCnt = 3;
+		func->paramStackSize = 0x18;
+
+		struct ba_FuncParam* params[3];
+
+		// addr (RDI)
+		params[0] = ba_NewFuncParam();
+		{
+			struct ba_Type* fundType = malloc(sizeof(*fundType));
+			fundType->type = BA_TYPE_VOID;
+			params[0]->type = (struct ba_Type){ BA_TYPE_PTR, fundType };
+		}
+		params[0]->hasDefaultVal = 0;
+		func->firstParam = params[0];
+
+		// size (RSI)
+		params[1] = ba_NewFuncParam();
+		params[1]->type = (struct ba_Type){ BA_TYPE_U64, 0 };
+		params[1]->hasDefaultVal = 0;
+		params[0]->next = params[1];
+
+		// prot (RDX)
+		params[2] = ba_NewFuncParam();
+		params[2]->type = (struct ba_Type){ BA_TYPE_I64, 0 };
+		params[2]->hasDefaultVal = 0;
+		params[1]->next = params[2];
 	}
 	if (!ba_BltinFlagsTest(BA_BLTIN_SysMUnmap)) {
 		struct ba_Func* func = ba_IncludeAddFunc(ctr, line, col, "MUnmap");
