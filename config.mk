ab4e3b84c9cb618a0c4ce705056b6b3538144f83
nikolashn <nehn.2019@gmail.com>
2022-08-10 16:20:21 +0100

    Add -march=native back to the list of cflags

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ VERSION = 1.0.0
 # C compiler
 CC = cc
 # C compiler flags
-CFLAGS = -std=gnu99 -O2 -Wall -Wshadow -Wno-unused-value -static
+CFLAGS = -std=gnu99 -O2 -march=native -Wall -Wshadow -Wno-unused-value -static
 CFLAGSDEBUG = -std=gnu99 -O0 -Wall -Wshadow
 CFLAGSPROFILE = -std=gnu99 -O0 -Wall -Wshadow
 # Path
